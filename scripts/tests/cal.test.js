f06029ae77fcca5aebb23a469849f99e63fb9770
AleksandrDenis <deniskudriavcev@gmail.com>
2022-07-23 12:18:07 +0000

    Create test for addition function

---
+++ scripts/tests/cal.test.js
@@ -0,0 +1,25 @@
+const addition = require("../calc");
+
+describe("Calculetor", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
+
+    });
+
+    describe("Substraction function", () => {
+
+    });
+
+    describe("Multiplycation function", () => {
+
+    });
+
+    describe("Division function", () => {
+
+    });
+})
